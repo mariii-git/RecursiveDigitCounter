48fd9811eaeecc537ccb8d6a734741e5d7eadcf5
mariiiiiii <masum.s1343@gmail.com>
2025-07-30 02:11:58 -0700

    arraysum

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+static int digitcount(int n)
+{
+    if (n < 10)
+        return 1;
+    else
+        return 1 + digitcount(n / 10);
+}
+Console.WriteLine(digitcount(1234));
